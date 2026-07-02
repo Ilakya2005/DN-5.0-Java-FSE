c11f33baa8e310a9b3b432a4715359e6f3d75f81
Ilakya2005 <ilakya.r2005@gmail.com>
2026-07-02 23:02:54 +0530

    SingletonPatternExample

---
+++ OneDrive/Documents/week1/SingletonPatternExample/TestLogger.java
@@ -0,0 +1,19 @@
+package SingletonPatternExample;
+
+public class TestLogger {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First Message");
+        logger2.log("Second Message");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance is created.");
+        } else {
+            System.out.println("Multiple instances are created.");
+        }
+    }
+}
